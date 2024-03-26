875cf8cd7c84c1ef9b022daa33ef8fbd45831c74
Bilyana Indzheva <bilyana2002@gmail.com>
2024-03-26 23:07:50 +0200

    Add test with 10 trees

--- onnxruntime/test/framework/inference_session_test.cc
+++ onnxruntime/test/framework/inference_session_test.cc
@@ -3056,12 +3056,16 @@ TEST(InferenceSessionTests, BenchSameMode1) {
     benchmarkModel(10, 5, "model_single_tree.onnx", "input_single_tree.txt");
 }
 
+TEST(InferenceSessionTests, BenchSameMode10) {
+    benchmarkModel(10, 5, "model_10_trees.onnx", "input.txt");
+}
+
 TEST(InferenceSessionTests, BenchSameMode100) {
     benchmarkModel(10, 5, "model.onnx", "input.txt");
 }
 
 TEST(InferenceSessionTests, BenchSameMode1000) {
-    benchmarkModel(10, 15, "model_samemode_big2.onnx", "input3.txt");
+    benchmarkModel(10, 5, "model_1000_trees.onnx", "input_1000_trees.txt");
 }
 
 TEST(InferenceSessionTests, BenchNotSameMode100) {
